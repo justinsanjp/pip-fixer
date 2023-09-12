697e80cb165c059c07258d72c7a34589a2d164de
Justinsanjp <64624657+justinsanjp@users.noreply.github.com>
2023-09-12 16:22:01 +0200

    Add files via upload

---
+++ pip-fixer.bat
@@ -0,0 +1,6 @@
+@echo off
+copy "C:\Program Files\Python311\Scripts\pip.exe" "C:\Windows\"
+copy "C:\Program Files\Python311\Scripts\pip3.exe" "C:\Windows\"
+copy "C:\Program Files\Python311\Scripts\pip3.11.exe" "C:\Windows\"
+echo Fix Complete.
+pause
